53d66bae6a70a269477d0dd9e65bce86d31e7d12
smandoki <80867148+smandoki@users.noreply.github.com>
2022-10-10 18:39:23 +0800

    Added link active logic
    Added functionality for clicked link to give itself the active class
    and styled the header. Also implemented style importing with webpack

--- src/header/header.js
+++ src/header/header.js
@@ -1,9 +1,11 @@
+import './header.style.css';
+
 const header = document.createElement('header');
 header.innerHTML = (`
     <h1>The Pizza Factory</h1>
     <nav>
         <ul>
-            <li>Home</li>
+            <li class="active">Home</li>
             <li>Menu</li>
             <li>Contact</li>
         </ul>
--- src/index.js
+++ src/index.js
@@ -1,4 +1,19 @@
-import header from "./header";
+import './style.css';
+import header from "./header/header";
+import home from "./home/home";
+import menu from "./menu/menu";
+import contact from "./contact/contact";
 
 const content = document.querySelector('#content');
 content.appendChild(header);
+
+
+//implement links to load in content modules
+const links = document.querySelectorAll('li');
+
+links.forEach(link => {
+    link.addEventListener('click', () => {
+        links.forEach(link => link.classList.remove('active'));
+        link.classList.add('active');
+    });
+});
