248205d553d85386774d2d35bb9320d9a92b8a8d
smandoki <80867148+smandoki@users.noreply.github.com>
2022-10-10 14:20:27 +0800

    Completed header component
    Completed header component. Added empty files for the following
    components: home, menu, and contact

---
+++ src/header.js
@@ -0,0 +1,13 @@
+const header = document.createElement('header');
+header.innerHTML = (`
+    <h1>The Pizza Factory</h1>
+    <nav>
+        <ul>
+            <li>Home</li>
+            <li>Menu</li>
+            <li>Contact</li>
+        </ul>
+    </nav>
+`);
+
+export default header;
---
+++ src/index.js
@@ -0,0 +1,4 @@
+import header from "./header";
+
+const content = document.querySelector('#content');
+content.appendChild(header);
